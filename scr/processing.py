4aba54b7f0ab46a6c2fa5efe15a56e2c0fd1801d
Альберт Хабиров <alb.xab@yandex.ru>
2026-02-17 18:19:41 +0500

    completing task 10_1

--- scr/processing.py
+++ scr/processing.py
@@ -1,15 +1,13 @@
-def filter_by_state(work_list: list, state='EXECUTED') -> list:
-    ''' Функция для фильтрации списка словарей по ключу'''
+def filter_by_state(work_list: list, state="EXECUTED") -> list:
+    """Функция для фильтрации списка словарей по ключу"""
     filtered_list = []
     for i in work_list:
-        if i['state'] == state:
+        if i["state"] == state:
             filtered_list.append(i)
     return filtered_list
 
 
 def sort_by_date(work_list: list, reverse=True) -> list:
-    '''Функция для сортировки списка по дате'''
-    filtered_list = []
-    filtered_list = sorted(work_list, key=lambda x: x['date'], reverse=reverse)
+    """Функция для сортировки списка по дате"""
+    filtered_list = sorted(work_list, key=lambda x: x["date"], reverse=reverse)
     return filtered_list
-
