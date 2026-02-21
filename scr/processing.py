b3c28e953d7db8355035c80caf98a29e3dea6eca
Альберт Хабиров <alb.xab@yandex.ru>
2026-02-21 10:00:07 +0500

    modification

--- scr/processing.py
+++ scr/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(work_list: list, state="EXECUTED") -> list:
+def filter_by_state(work_list: list, state: str = "EXECUTED") -> list:
     """Функция для фильтрации списка словарей по ключу"""
     filtered_list = []
     for i in work_list:
@@ -7,7 +7,7 @@ def filter_by_state(work_list: list, state="EXECUTED") -> list:
     return filtered_list
 
 
-def sort_by_date(work_list: list, reverse=True) -> list:
+def sort_by_date(work_list: list, reverse: bool = True) -> list:
     """Функция для сортировки списка по дате"""
     filtered_list = sorted(work_list, key=lambda x: x["date"], reverse=reverse)
     return filtered_list
